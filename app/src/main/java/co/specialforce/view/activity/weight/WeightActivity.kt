ef087826e42a93c6419588c4146e9acc53eb7134
LieutenantKang <blizzard1107@gmail.com>
2020-10-22 04:14:35 +0900

    Implement Weight Input View

--- app/src/main/java/co/specialforce/view/activity/weight/WeightActivity.kt
+++ app/src/main/java/co/specialforce/view/activity/weight/WeightActivity.kt
@@ -1,5 +1,6 @@
 package co.specialforce.view.activity.weight
 
+import android.content.Intent
 import android.view.View
 import co.specialforce.R
 import co.specialforce.base.BaseActivity
@@ -21,7 +22,18 @@ class WeightActivity : BaseActivity(), WeightContract.View, View.OnClickListener
     override fun onClick(v: View) {
         when(v.id){
             R.id.weight_input_button -> {
+                startActivityForResult(Intent(this,
+                    WeightDialogActivity::class.java),7777)
+            }
+        }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==7777){
+            if(resultCode==1){ // result = OK
+                val result : Float? = data?.getFloatExtra("Result", Float.MIN_VALUE)
+                // 서버 통신 필요
             }
         }
     }
--- app/src/main/java/co/specialforce/view/activity/weight/WeightDialogActivity.kt
+++ app/src/main/java/co/specialforce/view/activity/weight/WeightDialogActivity.kt
@@ -1,17 +1,51 @@
 package co.specialforce.view.activity.weight
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import co.specialforce.R
 import kotlinx.android.synthetic.main.activity_weight_dialog.*
 
-class WeightDialogActivity : AppCompatActivity() {
-
+class WeightDialogActivity : Activity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weight_dialog)
-        weight_dialog_picker.minValue = 20
-        weight_dialog_picker.maxValue = 150
-        weight_dialog_picker.value = 70
+
+        weight_dialog_picker.minValue=40
+        weight_dialog_picker.maxValue=120
+        weight_dialog_picker.value=70
+        weight_dialog_picker.wrapSelectorWheel = true
+
+//
+//        val list = ArrayList<String>()
+//        for(i in 40 until 120){
+//            for(j in 0 until 9){
+//                list.add("$i.$j")
+//            }
+//        }
+//
+//        weight_dialog_picker.displayedValues = list.toTypedArray()
+//        weight_dialog_picker.wrapSelectorWheel = false
+
+        weight_dialog_input_button.setOnClickListener(this)
+        weight_dialog_cancel_button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.weight_dialog_input_button -> {
+                val intent = Intent()
+                intent.putExtra("Result", weight_dialog_picker.value.toFloat())
+                setResult(1, intent)
+                finish()
+            }
+            R.id.weight_dialog_cancel_button -> finish()
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return event?.action != MotionEvent.ACTION_OUTSIDE
     }
 }
